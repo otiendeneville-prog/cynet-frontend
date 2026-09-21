b94f5acfd6ef0c8492cd0c4ea4015cd566c6d34d
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-21 11:00:01 +0300

    add testimals

--- src/components/ui/testimonials.tsx
+++ src/components/ui/testimonials.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function testimonials() {
-  return (
-    <section>
-        
-    </section>
-  )
+  return <section></section>;
 }
