53516c7333545f2b6fbe89f7ee4c4b92fa1a7b7f
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-21 12:35:36 +0300

    add the coursel

--- src/components/feedbackCard.tsx
+++ src/components/feedbackCard.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from "react";
 
-export default function feedBack({ content,name,title,img}) {
+export default function feedBack({ id, content, name, title, img }) {
   return (
     <>
-       <div className='justify-center flex-col flex px-10 py-12 rounde-[20px] max-w-[370px] max-auto my-0 feedback-card'>
-        <img src="https://i.imgur.com/rx3eOUo.png" alt="Double quotes" className='w-[42.6] h-[27.6] object-contain' />
-        <p className='justify-center align-center flex text-white font-poppin font-normal text-[19px]'>
+      <div className="justify-center flex-col flex px-10 py-12 rounde-[20px] max-w-[370px] max-auto my-0 feedback-card">
+        <img
+          src="https://i.imgur.com/rx3eOUo.png"
+          alt="Double quotes"
+          className="w-[42.6] h-[27.6] object-contain"
+        />
+        <p className="justify-center align-center flex text-white font-poppin font-normal text-[19px]">
           {content}
         </p>
-       </div>
-       <div className='flex  flex-row'>
+      </div>
+      <div className="flex  flex-row">
         <img className="rounded-full" src={img} alt={name} />
         {title}
-       </div>
-   </>
-      )
+        <div className="flex flex-col ml-4 ">
+          <h4 className="font-popping font-semibold text-[20px] leading-[32px] text-white flex-column ml-2">
+            {name}
+          </h4>
+          <p className="font-poppins font-normal text-[15px] leading-[24px] text-deemWhite">
+            {title}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/site/Testimonial.tsx
+++ src/components/site/Testimonial.tsx
@@ -1,7 +1,63 @@
-import React from "react";
 
-function Testimonial() {
-  return <div>Testimonial</div>;
-}
+import {feedbackCard} from "./feedbackCard";
+import {Carousel} from 'nuka-carousel';
 
-export default Testimonial;
+const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: "https://i.imgur.com/Dn0qoCG.png",
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: "https://i.imgur.com/fk8eEvW.png",
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: "https://i.imgur.com/dLxxRDy.png",
+  },
+];
+
+const Testimonials = () => (
+  <section
+    id="clients"
+    className={`sm:py-16 py-6 flex justify-center items-center flex-col relative `}
+  >
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2
+        className={`font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full`}
+      >
+        What People are <br className="sm:block hidden" /> saying about us
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p
+          className={`font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-left max-w-[450px]`}
+        >
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+    <Carousel autoplay autoplayInterval={3000} wrapAround={true}>
+      {feedback.map((card) => (
+        <feedbackCard key={card.id} {...card} />
+      ))}
+    </Carousel>
+  </section>
+);
+
+export default Testimonials;
+
