e10a91347fd120e90f1d1c7adbe0b15210bfeeca
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-23 12:38:09 +0300

    add the delivery results across east africa

--- src/components/site/OurServices.tsx
+++ src/components/site/OurServices.tsx
@@ -11,13 +11,13 @@ export default function OurServices() {
                 Services
                 </span>
             </h2>
-            <div className="ml-20 align-center justify-center content-center">
+            <div className="ml-20 align-center justify-center content-end inline-6xl">
                 <p>
                 Tailored solutions to help organizations 
                 succeed — from growth <br /> strategy and financial advisory to team building and research. We drive <br /> measurable outcomes at every stage.
                 </p>
-                <button>
-                    <h1 className="text-2xl mr-16">
+                <button className="mt-10 border-l-purple-50 rounded-2xl">
+                    <h1>
                         Years of experience
                     </h1>
                     <p>Delivering results across East Africa</p>
