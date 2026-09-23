b8f2e7047a4bef8a315c736933dd8cfb96e50f09
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-23 11:55:48 +0300

    adding heading

--- src/components/site/OurServices.tsx
+++ src/components/site/OurServices.tsx
@@ -3,15 +3,26 @@
 export default function OurServices() {
   return (
     <section className="relative overflow-hidden border-border  flex items-center">
-        <div>
+        <div className="align-center flex justify-center">
             <h2 className="font-bold ml-9 text-secondary text-3xl lg:text-4xl mt-2">
               
                 Our 
                 <span className="text-primary ml-2">
-                     Services
+                Services
                 </span>
-               
             </h2>
+            <div className="ml-20 align-center justify-center content-center">
+                <p>
+                Tailored solutions to help organizations 
+                succeed — from growth <br /> strategy and financial advisory to team building and research. We drive <br /> measurable outcomes at every stage.
+                </p>
+                <button>
+                    <h1 className="text-2xl mr-16">
+                        Years of experience
+                    </h1>
+                    <p>Delivering results across East Africa</p>
+                </button>
+            </div>
         </div>
     </section>
   )
